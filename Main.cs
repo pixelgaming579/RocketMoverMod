eac6b8aabcafa39b40dc986c0f0e3afad882b576
pixelgaming579 <jack.pryde@outlook.com>
2022-03-17 08:02:19 +1100

    2.0 beta

--- Main.cs
+++ Main.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections;
+// using System.Collections;
 using UnityEngine;
 using ModLoader;
 using HarmonyLib;
 using SFS;
+using SFS.WorldBase;
 
 namespace RocketMoverMod
 {
--- RocketMoverMenu.cs
+++ RocketMoverMenu.cs
@@ -1,23 +1,34 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using ModLoader;
-using HarmonyLib;
 using SFS.World;
+using SFS.WorldBase;
 using SFS.Input;
 using SFS.UI;
-using SFS.Parts.Modules;
+using static SFS.Base;
 
 namespace RocketMoverMod
 {
     public class RocketMoverMenu : MonoBehaviour
     {
+        Dictionary<string, Planet> planets = planetLoader.planets;
+        List<string> planetNames = new List<string>();
+        private void Start()
+        {
+            foreach (string planetName in planets.Keys)
+            {
+                this.planetNames.Add(planetName);
+            };
+        }
         private static int menuWidth = 200;
         private int borderThickness = 10;
+        int selectedPlanetIndex;
+        float changePlanetScrollBar = 0;
         string currentSubmenu = "";
-        float RocketMoverX = 1f;
-        float RocketMoverY = 1f;
-        float RocketMoverR = 90f;
+        float RocketMoverX = 5f;
+        float RocketMoverY = 5f;
+        float RocketMoverR = 1f;
         public Rect menuGUI = new Rect(10, 50, menuWidth, 130);
         public Rect submenuGUI = new Rect(10, 190, menuWidth, 260);
         void OnGUI()
@@ -50,10 +61,24 @@ namespace RocketMoverMod
                 }
                 if (GUI.Button(new Rect(borderThickness, 70, menuWidth - (borderThickness*2), 20), "Change Planet"))
                 {
+                    if (this.currentSubmenu == "Move Rocket")
+                    {
+                        WorldTime.main.SetState(1, true, false);
+                        MsgDrawer.main.Log("Physics resumed");
+                        Rocket playerRocket = PlayerController.main.player.Value as Rocket;
+                        playerRocket.rb2d.constraints = RigidbodyConstraints2D.None;
+                    }
                     this.currentSubmenu = "Change Planet";
                 }
                 if (GUI.Button(new Rect(borderThickness, 100, menuWidth - (borderThickness*2), 20), "Set Orbit"))
                 {
+                    if (this.currentSubmenu == "Move Rocket")
+                    {
+                        WorldTime.main.SetState(1, true, false);
+                        MsgDrawer.main.Log("Physics resumed");
+                        Rocket playerRocket = PlayerController.main.player.Value as Rocket;
+                        playerRocket.rb2d.constraints = RigidbodyConstraints2D.None;
+                    }
                     this.currentSubmenu = "Set Orbit";
                 }
                 GUI.DragWindow();
@@ -64,15 +89,15 @@ namespace RocketMoverMod
             {
                 GUI.Label(new Rect(borderThickness, 20, menuWidth - (borderThickness*2), 80), "Use your RCS controls to move and rotate the rocket. Close this menu to resume the rocket's physics.");
 
-                RocketMoverX = GUI.HorizontalSlider(new Rect(borderThickness, 100, menuWidth - (borderThickness*2), 20), RocketMoverX, 0.1f, 100f);
+                RocketMoverX = GUI.HorizontalSlider(new Rect(borderThickness, 100, menuWidth - (borderThickness*2), 20), RocketMoverX, 0.1f, 25f);
                 GUI.Box(new Rect(borderThickness, 100, menuWidth - (borderThickness*2), 20), new GUIContent("", "Horziontal speed"), GUIStyle.none);
                 Rect rectx = new Rect(borderThickness, 80, menuWidth - (borderThickness*2), 20);
 
-                RocketMoverY = GUI.HorizontalSlider(new Rect(borderThickness, 130, menuWidth - (borderThickness*2), 20), RocketMoverY, 0.1f, 50f);
+                RocketMoverY = GUI.HorizontalSlider(new Rect(borderThickness, 130, menuWidth - (borderThickness*2), 20), RocketMoverY, 0.1f, 15f);
                 GUI.Box(new Rect(borderThickness, 130, menuWidth - (borderThickness*2), 20), new GUIContent("", "Vertical speed"), GUIStyle.none);
                 Rect recty = new Rect(borderThickness, 110, menuWidth - (borderThickness*2), 20);
 
-                RocketMoverR = GUI.HorizontalSlider(new Rect(borderThickness, 160, menuWidth - (borderThickness*2), 20), RocketMoverR, 0.1f, 90f);
+                RocketMoverR = GUI.HorizontalSlider(new Rect(borderThickness, 160, menuWidth - (borderThickness*2), 20), RocketMoverR, 0.1f, 5f);
                 GUI.Box(new Rect(borderThickness, 160, menuWidth - (borderThickness*2), 20), new GUIContent("", "Rotation speed"), GUIStyle.none);
                 Rect rectr = new Rect(borderThickness, 140, menuWidth - (borderThickness*2), 20);
 
@@ -104,7 +129,12 @@ namespace RocketMoverMod
             }
             else if (this.currentSubmenu == "Change Planet")
             {
-
+                GUI.Label(new Rect(borderThickness, 20, menuWidth - (borderThickness*2), 50), "Select a planet from the list below, then press the \"Teleport to\" button to teleport.");
+                if (GUI.Button(new Rect(borderThickness, 20, menuWidth - (borderThickness*2), 20), "Teleport to "+this.planetNames[selectedPlanetIndex]))
+                {
+                    ChangePlanet();
+                }
+                selectedPlanetIndex = GUI.SelectionGrid(new Rect(borderThickness, 100, menuWidth - (borderThickness*2), 180), selectedPlanetIndex, this.planetNames.ToArray(), 2);
             }
             else if (this.currentSubmenu == "Set Orbit")
             {
@@ -125,54 +155,61 @@ namespace RocketMoverMod
             Rocket playerRocket = PlayerController.main.player.Value as Rocket;
             if (playerRocket)
             {
-                // RcsModule[] rcsParts = playerRocket.partHolder.GetModules<RcsModule>();
-                // foreach (RcsModule rcs in rcsParts)
-                // {
-                //     rcs.RCS_On = fals
-                // }
                 Location rocketLocation = playerRocket.location.Value;
                 Vector2 transform = playerRocket.rb2d.position;
                 playerRocket.rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
-                if (Input.GetKeyDown(KeybindingsPC.keys.Move_Rocket_Using_RCS[0].key)) // Up direction.
+                if (Input.GetKey(KeybindingsPC.keys.Move_Rocket_Using_RCS[0].key)) // Up direction.
                 {
-                    // print("w");
                     Quaternion preRot = playerRocket.rb2d.transform.localRotation;
                     playerRocket.rb2d.transform.localRotation = Quaternion.identity;
                     playerRocket.rb2d.transform.Translate(0, speedY, 0);
                     playerRocket.rb2d.transform.localRotation = preRot;
                 }
-                else if (Input.GetKeyDown(KeybindingsPC.keys.Move_Rocket_Using_RCS[1].key)) // Left direction.
+                else if (Input.GetKey(KeybindingsPC.keys.Move_Rocket_Using_RCS[2].key)) // Down direction.
                 {
-                    // print("a");
-                    Quaternion preRot = playerRocket.rb2d.transform.localRotation;
-                    playerRocket.rb2d.transform.localRotation = Quaternion.identity;
-                    playerRocket.rb2d.transform.Translate(-speedX, 0, 0);
-                    playerRocket.rb2d.transform.localRotation = preRot;
-                }
-                else if (Input.GetKeyDown(KeybindingsPC.keys.Move_Rocket_Using_RCS[2].key)) // Down direction.
-                {
-                    // print("s");
                     Quaternion preRot = playerRocket.rb2d.transform.localRotation;
                     playerRocket.rb2d.transform.localRotation = Quaternion.identity;
                     playerRocket.rb2d.transform.Translate(0, -speedY, 0);
                     playerRocket.rb2d.transform.localRotation = preRot;
                 }
-                else if (Input.GetKeyDown(KeybindingsPC.keys.Move_Rocket_Using_RCS[3].key)) // Right direction.
+                if (Input.GetKey(KeybindingsPC.keys.Move_Rocket_Using_RCS[1].key)) // Left direction.
                 {
-                    // print("d");
+                    Quaternion preRot = playerRocket.rb2d.transform.localRotation;
+                    playerRocket.rb2d.transform.localRotation = Quaternion.identity;
+                    playerRocket.rb2d.transform.Translate(-speedX, 0, 0);
+                    playerRocket.rb2d.transform.localRotation = preRot;
+                }
+                else if (Input.GetKey(KeybindingsPC.keys.Move_Rocket_Using_RCS[3].key)) // Right direction.
+                {
                     Quaternion preRot = playerRocket.rb2d.transform.localRotation;
                     playerRocket.rb2d.transform.localRotation = Quaternion.identity;
                     playerRocket.rb2d.transform.Translate(speedX, 0, 0);
                     playerRocket.rb2d.transform.localRotation = preRot;
                 }
-                if (Input.GetKeyDown(KeybindingsPC.keys.Turn_Rocket[0].key))
+                if (Input.GetKey(KeybindingsPC.keys.Turn_Rocket[0].key))
                 {
                     playerRocket.rb2d.transform.Rotate(new Vector3(0, 0, speedR));
                 }
-                else if (Input.GetKeyDown(KeybindingsPC.keys.Turn_Rocket[1].key))
+                else if (Input.GetKey(KeybindingsPC.keys.Turn_Rocket[1].key))
                 {
                     playerRocket.rb2d.transform.Rotate(new Vector3(0, 0, -speedR));
                 } 
+            }
+        }
+        public void ChangePlanet()
+        {
+            string planetName = planetNames[selectedPlanetIndex];
+            Planet planet = planets[planetName];
+            if (PlayerController.main.player.Value as Rocket)
+            {
+                SFS.Variables.Planet_Local planet_local = new SFS.Variables.Planet_Local();
+                planet_local.Value = planet;
+                WorldLocation newLocation = new WorldLocation();
+                newLocation.planet = planet_local;
+                SFS.Variables.Double2_Local pos = new SFS.Variables.Double2_Local();
+                pos.Value = new Double2(0, planet.GetTerrainHeightAtAngle(0.1*Mathf.Deg2Rad));
+                newLocation.position = pos;
+                (PlayerController.main.player.Value as Rocket).physics.SetLocationAndState(newLocation.Value, (PlayerController.main.player.Value as Rocket).physics.PhysicsMode);
             }
         }
     }
